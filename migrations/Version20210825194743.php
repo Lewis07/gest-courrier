af467076d242ecfc2da6ab6e12d677f4274d6f51
Freddino <onjatianafreddino@gmail.com>
2021-08-25 23:14:30 +0300

    Gestion Courrier

--- migrations/Version20210825194743.php
+++ migrations/Version20210825194743.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210825083446 extends AbstractMigration
+final class Version20210825194743 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,9 @@ final class Version20210825083446 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE partage_courrier (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE partage_courrier (id INT AUTO_INCREMENT NOT NULL, sender_id INT NOT NULL, INDEX IDX_32BF4631F624B39D (sender_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE partage_courrier_user (partage_courrier_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_D1FADDD9BE150C3B (partage_courrier_id), INDEX IDX_D1FADDD9A76ED395 (user_id), PRIMARY KEY(partage_courrier_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE partage_courrier ADD CONSTRAINT FK_32BF4631F624B39D FOREIGN KEY (sender_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE partage_courrier_user ADD CONSTRAINT FK_D1FADDD9BE150C3B FOREIGN KEY (partage_courrier_id) REFERENCES partage_courrier (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE partage_courrier_user ADD CONSTRAINT FK_D1FADDD9A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
     }
