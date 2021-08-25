7d6d8310ceceab5f59513bfc07f09dc0b0c6cf7b
Lewis07 <rabarison.lewis@yahoo.com>
2021-08-25 23:33:58 +0300

    profil

--- src/Controller/FrontOffice/ProfileController.php
+++ src/Controller/FrontOffice/ProfileController.php
@@ -2,19 +2,40 @@
 
 namespace App\Controller\FrontOffice;
 
+use App\Form\ProfileType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
-    /**
-     * @Route("/profile", name="profile")
-     */
-    public function index(): Response
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/profile", name="Profile")
+     */
+    public function index(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileType::class,$user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+            $this->addFlash('success',"Votre compte est modifié avec succès");
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('FrontOffice/Profile/index.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
