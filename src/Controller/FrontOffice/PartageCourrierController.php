2fad0fc100d345e7e93da09d38a6e03d12522809
Lewis07 <rabarison.lewis@yahoo.com>
2021-09-07 15:59:29 +0300

    courrier partager

--- src/Controller/FrontOffice/PartageCourrierController.php
+++ src/Controller/FrontOffice/PartageCourrierController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\FrontOffice;
 
+use App\Entity\Courrier;
 use App\Entity\PartageCourrier;
 use App\Form\PartageCourrierType;
+use App\Repository\CourrierRepository;
 use App\Repository\PartageCourrierRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,27 +20,37 @@ class PartageCourrierController extends AbstractController
 {
     private $em;
     private $partageCourrierRepository;
+    private $courrierRepository;
 
     /**
      * PartageCourrierController constructor.
      * @param EntityManagerInterface $em
      * @param PartageCourrierRepository $partageCourrierRepository
+     * @param CourrierRepository $courrierRepository
      */
-    public function __construct(EntityManagerInterface $em, PartageCourrierRepository $partageCourrierRepository)
+    public function __construct(EntityManagerInterface $em, PartageCourrierRepository $partageCourrierRepository,
+                                CourrierRepository $courrierRepository)
     {
         $this->em = $em;
         $this->partageCourrierRepository = $partageCourrierRepository;
+        $this->courrierRepository = $courrierRepository;
     }
     /**
      * @Route("/", name="partage_courrier_index")
      */
     public function index(): Response
     {
-        if ($this->getUser()){
+        /*if ($this->getUser()){
             $partage_courriers = $this->partageCourrierRepository->partageCourrierUtilisateur($this->getUser()->getId());
+        }*/
+
+        $partageurs = $this->partageCourrierRepository->findBy(['sender' => $this->getUser()]);
+        $partage_courriers = [];
+
+        foreach ($partageurs as $partageur){
+            $sender_id = $partageur->getSender()->getId();
+            $partage_courriers = $this->courrierRepository->findBy(['sender' => $sender_id,'isInTrashed' => 0,'isShared' => 1]);
         }
-//
-//        dd($partage_courriers);
 
         return $this->render('FrontOffice/Courrier/Partage/partage.html.twig', compact('partage_courriers'));
     }
@@ -58,7 +70,6 @@ class PartageCourrierController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid() ){
-            // dd($this->getUser());
             $_recipients = $form->get("recepteur_courrier_partage")->getData();
             $partage_courrier->setSender($this->getUser());
 
@@ -66,7 +77,11 @@ class PartageCourrierController extends AbstractController
                 $partage_courrier->addSharer($shared);
             }
 
+            $courrier = $this->courrierRepository->find($courrier_id);
+            $courrier->setIsShared(true);
+
             $this->em->persist($partage_courrier);
+            $this->em->persist($courrier);
             $this->em->flush();
 
             $this->addFlash("success", "message envoyé avec succès");
@@ -78,4 +93,27 @@ class PartageCourrierController extends AbstractController
             'form' => $form->CreateView()
         ]);
     }
+
+    /**
+     * Suppression du courrier vers corbeille
+     * @Route("/{id}/delete-to-trash-partage", name="courrier_delete_trash_share")
+     * @param Courrier $courrier
+     * @param Request $request
+     * @param $route_name
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function deleteInTrash(Courrier $courrier, Request $request)
+    {
+        if (!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
+
+        $courrier->setIsInTrashed(true);
+        $this->em->persist($courrier);
+
+        $this->em->flush();
+
+        return $this->redirectToRoute('partage_courrier_index');
+    }
+
 }
