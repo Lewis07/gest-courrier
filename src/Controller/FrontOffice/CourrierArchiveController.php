8efb94f16606ea4ccfa3ca040826ab2150838050
Lewis07 <rabarison.lewis@yahoo.com>
2021-09-07 17:21:28 +0300

    rectification suppression archive

--- src/Controller/FrontOffice/CourrierArchiveController.php
+++ src/Controller/FrontOffice/CourrierArchiveController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\FrontOffice;
 
 use App\Entity\Courrier;
+use App\Repository\CourrierArchiveRepository;
+use App\Repository\CourrierRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,14 +14,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class CourrierArchiveController extends AbstractController
 {
     private $em;
+    private $courrierRepository;
+    private $courrierArchiveRepository;
 
     /**
      * CourrierArchiveController constructor.
      * @param EntityManagerInterface $em
+     * @param CourrierRepository $courrierRepository
+     * @param CourrierArchiveRepository $courrierArchiveRepository
      */
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, CourrierRepository $courrierRepository,
+                                CourrierArchiveRepository $courrierArchiveRepository)
     {
         $this->em = $em;
+        $this->courrierRepository = $courrierRepository;
+        $this->courrierArchiveRepository = $courrierArchiveRepository;
     }
 
     /**
@@ -30,14 +39,16 @@ class CourrierArchiveController extends AbstractController
      * @param $route_name
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
-    public function deleteInTrash(Courrier $courrier, Request $request)
+    public function deleteInTrash(Courrier $courrier, Request $request, $id)
     {
         if (!$this->getUser()){
             return $this->redirectToRoute('app_login');
         }
 
-        $courrier->setIsInTrashed(true);
-        $this->em->persist($courrier);
+        $courrier_archive = $this->courrierArchiveRepository->find($id);
+
+        $courrier_archive->setIsInTrashed(true);
+        $this->em->persist($courrier_archive);
 
         $this->em->flush();
 
